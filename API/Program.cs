7c41d5919ee573312207b03eab25460f6c5c38cd
sanath nath <sanathnadh54@gmail.com>
2023-02-03 17:43:28 +0530

    middle of section 8

--- API/Program.cs
+++ API/Program.cs
@@ -33,4 +33,16 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch(Exception e){
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(e, "An error occured during migration");
+}
+
 app.Run();
