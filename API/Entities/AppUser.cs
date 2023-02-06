726703a196a362ffdbce98e0a0d99618a2702587
sanath nath <sanathnadh54@gmail.com>
2023-02-06 11:21:23 +0530

    end of section 8

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -40,8 +40,8 @@ namespace API.Entities
 
         public List<Photo> Photos { get; set; } = new();
 
-        public int GetAge(){
-            return DateOfBirth.CalculateAge();
-        }
+        // public int GetAge(){
+        //     return DateOfBirth.CalculateAge();
+        // }
     }
 }
